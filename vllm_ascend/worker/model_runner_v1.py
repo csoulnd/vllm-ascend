13666313ffcb1279cf9b58de00a497aa08aeda70
csoulnd <daidaicurry@foxmail.com>
2026-04-17 18:36:33 +0800

    cleancode
    Signed-off-by: csoulnd <daidaicurry@foxmail.com>

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -631,20 +631,6 @@ class NPUModelRunner(GPUModelRunner):
                 dtype=np.int32,
             )
         attn_state = self._build_attn_state(num_reqs, num_scheduled_tokens, num_valid_tokens)
-        if (
-            self.speculative_config is not None
-            and self.speculative_config.method == "mtp"
-            and scheduler_output.scheduled_spec_decode_tokens
-        ):
-            logger.warning(
-                "[MTP_DEBUG][prepare_inputs] attn_state=%s scheduled_spec_decode_tokens=%s num_scheduled_tokens=%s",
-                attn_state,
-                dict(scheduler_output.scheduled_spec_decode_tokens),
-                {
-                    req_id: scheduler_output.num_scheduled_tokens.get(req_id, 0)
-                    for req_id in self.input_batch.req_ids
-                },
-            )
 
         # Determine if it's a splitfuse batch
         with_prefill = attn_state not in [AscendAttentionState.DecodeOnly, AscendAttentionState.SpecDecoding]
@@ -854,7 +840,6 @@ class NPUModelRunner(GPUModelRunner):
                 draft_token_ids,
             ) in scheduler_output.scheduled_spec_decode_tokens.items():
                 req_idx = self.input_batch.req_id_to_index[req_id]
-                raw_draft_len = len(draft_token_ids)
                 valid_draft_len = sum(1 for token in draft_token_ids if token >= 0)
                 num_draft_tokens[req_idx] = valid_draft_len
                 if (self.is_kv_consumer and req_id in new_schedule_reqs) or \
@@ -863,14 +848,6 @@ class NPUModelRunner(GPUModelRunner):
                     num_decode_draft_tokens[req_idx] = valid_draft_len
                 else:
                     num_decode_draft_tokens[req_idx] = -1
-                if self.speculative_config is not None and self.speculative_config.method == "mtp":
-                    logger.warning(
-                        "[MTP_DEBUG][draft_count] req_id=%s raw_draft_token_ids=%s raw_draft_len=%s valid_draft_len=%s",
-                        req_id,
-                        list(draft_token_ids),
-                        raw_draft_len,
-                        valid_draft_len,
-                    )
 
             spec_decode_metadata = self._calc_spec_decode_metadata(
                 num_draft_tokens,
@@ -1687,11 +1664,6 @@ class NPUModelRunner(GPUModelRunner):
             logits,
             sampling_metadata,
         )
-        if self.speculative_config is not None and self.speculative_config.method == "mtp":
-            logger.warning(
-                "[MTP_DEBUG][verify] sampled_token_ids(raw)=%s",
-                sampler_output.sampled_token_ids.tolist(),
-            )
         return sampler_output
 
     # TODO: remove this func after eagle_proposer is refactored and
@@ -1746,11 +1718,6 @@ class NPUModelRunner(GPUModelRunner):
                     discard_sampled_tokens_req_indices,
                     logprobs_tensors=logprobs_tensors,
                 )
-            if self.speculative_config is not None and self.speculative_config.method == "mtp":
-                logger.warning(
-                    "[MTP_DEBUG][verify] valid_sampled_token_ids=%s",
-                    valid_sampled_token_ids,
-                )
         else:
             valid_sampled_token_ids = []
             invalid_req_indices = discard_sampled_tokens_req_indices.tolist()
@@ -1763,11 +1730,6 @@ class NPUModelRunner(GPUModelRunner):
                     discard_sampled_tokens_req_indices,
                     logprobs_tensors=logprobs_tensors,
                 )
-                if self.speculative_config.method == "mtp":
-                    logger.warning(
-                        "[MTP_DEBUG][verify_async] valid_sampled_token_ids=%s",
-                        valid_sampled_token_ids,
-                    )
                 # MTP on 310P can run with placeholder drafts (-1). In this case
                 # the real accepted token is already materialized in sampled_token_ids,
                 # and must be cached for the next step instead of writing [-1].
@@ -1819,13 +1781,6 @@ class NPUModelRunner(GPUModelRunner):
             req_id = req_ids[req_idx]
             req_state = self.requests[req_id]
             req_state.output_token_ids.extend(sampled_ids)
-            if self.speculative_config is not None and self.speculative_config.method == "mtp":
-                logger.warning(
-                    "[MTP_DEBUG][commit] req_id=%s writeback_sampled_ids=%s output_token_ids_tail=%s",
-                    req_id,
-                    sampled_ids,
-                    req_state.output_token_ids[-16:],
-                )
 
         logprobs_lists = (
             logprobs_tensors.tolists(cu_num_tokens)
